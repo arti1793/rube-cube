18939becd5ea400c3a1a074ee94a679edfcbdc9d
Иван Митирев <arti1793@arti1793-MacBook-Pro.local>
2020-04-12 20:13:08 +0300

    test fix

--- src/algorithm/__Node__.test.ts
+++ src/algorithm/__Node__.test.ts
@@ -4,6 +4,9 @@ import { EColor, ECubeFace, ECubieSide } from '../common/CommonConstants';
 import { CubieMeta } from '../Cubes/CubieMeta/CubieMeta';
 import { CubieSide } from '../Cubes/CubieSide/CubieSide';
 import completedRaw from './__AlgoriithmCompletedCube__.test.json';
+import halfOfEdgesCompletedRaw from './__AlgorithmOnlyHalfOfEdgesCompleted__.test.json';
+import onlyTopsCompletedRaw from './__AlgorithmOnlyTopsCompleted__.test.json';
+import randomisedRaw from './__AlgorithmRandomisedCube__.test.json';
 import { Node } from './Node';
 
 const serializer = (
@@ -27,9 +30,60 @@ const serializer = (
   );
 };
 
-const COMPLETED = serializer(completedRaw);
+const COMPLETED: CubieMeta[] = serializer(completedRaw);
+const RANDOMISED: CubieMeta[] = serializer(randomisedRaw);
+const ONLY_TOPS: CubieMeta[] = serializer(onlyTopsCompletedRaw);
+const HALF_OF_EDGES: CubieMeta[] = serializer(halfOfEdgesCompletedRaw);
 
 test('Node. should create', () => {
   const node = new Node(COMPLETED, 0);
   expect(node).toBeInstanceOf(Node);
+});
+
+test('Node. isComplete should pass', () => {
+  const node: Node = new Node(COMPLETED, 0);
+  const isComplete = node.isComplete();
+  expect(isComplete).toBe(true);
+});
+
+test('Node. isComplete should not pass', () => {
+  const node: Node = new Node(RANDOMISED, 0);
+  const isComplete = node.isComplete();
+  expect(isComplete).toBe(false);
+});
+
+test('Node. areTopsComplete on completed cube should pass', () => {
+  const node: Node = new Node(COMPLETED, 0);
+  const areTopsComplete = node.areTopsComplete();
+  expect(areTopsComplete).toBe(true);
+});
+
+test('Node. areTopsComplete on randomised cube should not pass', () => {
+  const node: Node = new Node(RANDOMISED, 0);
+  const areTopsComplete = node.areTopsComplete();
+  expect(areTopsComplete).toBe(false);
+});
+
+test('Node. areTopsComplete on ONLY_TOPS should pass', () => {
+  const node: Node = new Node(ONLY_TOPS, 0);
+  const areTopsComplete = node.areTopsComplete();
+  expect(areTopsComplete).toBe(true);
+});
+
+test('Node. half of edges on completed should pass', () => {
+  const node: Node = new Node(COMPLETED, 0);
+  const isHalfOfEdgesComplete = node.isHalfOfEdgesComplete();
+  expect(isHalfOfEdgesComplete).toBe(true);
+});
+
+test('Node. half of edges on randomised should not pass', () => {
+  const node: Node = new Node(RANDOMISED, 0);
+  const isHalfOfEdgesComplete = node.isHalfOfEdgesComplete();
+  expect(isHalfOfEdgesComplete).toBe(false);
+});
+
+test('Node. half of edges on onlyHalfOfEdges should pass', () => {
+  const node: Node = new Node(HALF_OF_EDGES, 0);
+  const isHalfOfEdgesComplete = node.isHalfOfEdgesComplete();
+  expect(isHalfOfEdgesComplete).toBe(true);
 });
